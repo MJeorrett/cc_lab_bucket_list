49c7b3f1e2343c3f285711c3da00c106eb784c82
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-13 15:07:48 +0000

    fetching country names

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,17 @@
-var Bank = require('./bank/bank.js');
-var Account = require('./bank/account.js');
-// var sampleAccounts = require('./sample.json');
-var BankView = require('./views/bank_view.js');
-
 window.onload = function() {
-  var bank = new Bank();
-  var url = "http://localhost:3000/accounts";
+  var url = "https://restcountries.eu/rest/v1/all";
   var request = new XMLHttpRequest();
   request.open( 'GET', url );
-  request.onload = function() {
-    if ( request.status == 200 ) {
-      var sampleAccounts = JSON.parse( this.responseText );
 
-      for(account of sampleAccounts) {
-        bank.addAccount(new Account(account));
-      }
-      var bankView = new BankView(bank);
-      bankView.bindEvents();
-      bankView.render();
+  request.onload = function() {
+    if ( request.status === 200 ) {
+      var allCountriesData = JSON.parse( this.responseText );
+      console.log("allCountriesData:", allCountriesData);
+      var allCountryNames = allCountriesData.map( function( countryData ) {
+        console.log("countryData:", countryData);
+        return countryData.name;
+      });
+      console.log("allCountryNames:", allCountryNames);
     }
   };
   request.send();
