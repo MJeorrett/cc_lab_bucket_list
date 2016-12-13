66b0248465f1a626665104dda5ed28dcc5c07ea0
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-13 16:12:19 +0000

    finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,37 @@
-var MasterView = require('./views/masterView'); 
+var MasterView = require('./views/masterView');
 
 window.onload = function() {
-  var url = "https://restcountries.eu/rest/v1/all";
-  var request = new XMLHttpRequest();
-  request.open( 'GET', url );
 
-  request.onload = function() {
-    if ( request.status === 200 ) {
-      var allCountriesData = JSON.parse( this.responseText );
-      var allCountryNames = allCountriesData.map( function( countryData ) {
-        return countryData.name;
-      });
-      var masterView = new MasterView(allCountryNames, null);
-      masterView.populateSelect();
+  var bucketListUrl = "http://localhost:3000/bucket-list-countries";
+  var bucketListRequest = new XMLHttpRequest();
+  bucketListRequest.open( 'GET', bucketListUrl );
+
+  bucketListRequest.onload = function() {
+
+    if ( bucketListRequest.status == 200 ) {
+
+      var bucketListCountriesData = JSON.parse( this.responseText );
+
+      console.log( "bucketListCountriesData:", bucketListCountriesData );
+
+      var url = "https://restcountries.eu/rest/v1/all";
+      var request = new XMLHttpRequest();
+      request.open( 'GET', url );
+
+
+      request.onload = function() {
+        if ( request.status === 200 ) {
+          var allCountriesData = JSON.parse( this.responseText );
+          var allCountryNames = allCountriesData.map( function( countryData ) {
+            return countryData.name;
+          });
+          var masterView = new MasterView(allCountryNames, bucketListCountriesData );
+          masterView.populateSelect();
+          masterView.render();
+        }
+      };
+      request.send();
     }
   };
-  request.send();
+  bucketListRequest.send();
 };
--- client/src/views/masterView.js
+++ client/src/views/masterView.js
@@ -3,6 +3,17 @@ var MasterView = function(countryNames, bucketListData){
   this.bucketList = document.getElementById('bucket-list');
   this.countryNames = countryNames;
   this.bucketListData = bucketListData;
+  this.selectList.onchange = function( ev ) {
+    var countryName = ev.target.value;
+    var country = {
+      name: countryName,
+      alpha3Code: "undefined"
+    };
+
+    this.bucketListData.push( country );
+    this.render();
+    this.saveCountry( country );
+  }.bind( this );
 };
 
 MasterView.prototype = {
@@ -14,7 +25,32 @@ MasterView.prototype = {
       option.innerText = countryName;
       this.selectList.appendChild(option);
     }.bind(this));
+  },
+
+  render: function() {
+    this.bucketList.innerHTML = "";
+
+    this.bucketListData.forEach( function( countryData ) {
+      var li = document.createElement( 'li' );
+      li.innerText = countryData.name;
+      this.bucketList.appendChild( li );
+    }.bind( this ));
+  },
+
+  saveCountry: function( country ) {
+    var url = "http://localhost:3000/bucket-list-countries";
+    var request = new XMLHttpRequest();
+    request.open( 'POST', url );
+    request.setRequestHeader("Content-Type", "application/json");
+
+    request.onload = function() {
+      if ( request.status == 200 ) {
+        console.log("country added to bucket list:", country );
+      }
+    }
+    request.send( JSON.stringify( country ) );
   }
+
 };
 
 
